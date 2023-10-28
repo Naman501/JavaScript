a40c6b84785603b46be625d62d3133e66830f7be
Naman Mittal <namanmittal501@gmail.com>
2023-10-28 09:20:38 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+console.log(typeof "score");
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" =>33
+//"33abc"=> NaN
+//true => 1; false =>0
+//" =>false"
+//"value" => true
+
+let isLoggedIn = "nph"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
