7aafe846a10ca53b2c9a6a2b775ebe75c3f1f35d
Naman Mittal <namanmittal501@gmail.com>
2023-10-28 08:34:05 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1444532
+let accountEmail = "naman@gmail.com"
+var accountPassword = "Lawda_mera"
+accountCity = "Noida"
+
+// accountId = 2 //  not allowed
+
+accountEmail = "nm@nm.com"
+accountPassword = "LUND_lele"
+accountCity="Solan"
+
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+*/
