91c83299237ff8bc1d95576bb6ed79f25cf002e2
Naman Mittal <namanmittal501@gmail.com>
2023-10-28 08:44:56 +0000

    different Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version//
+
+// alert( 6+9)// we are using node js not browser
+
+console.log(2+9);
+console.log("NAMAN");
+
+let name = "Naman"
+let age = 20
+let isLoggedIn= false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null => standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof "Naman");
+console.log(typeof null);
+console.log(typeof age);
