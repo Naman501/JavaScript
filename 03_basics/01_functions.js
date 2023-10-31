bc4fb4de6b783db130ae53da01bb406b73225525
Naman Mittal <namanmittal501@gmail.com>
2023-10-31 04:59:22 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,3 +37,39 @@ function loginUserMessage(username = "Sam"){
 
 console.log(loginUserMessage("NAMAN"))
 console.log(loginUserMessage())
+
+function calculateCartPrice(val1,val2,...num1)// rest operator =>
+{
+return num1
+}
+
+console.log(calculateCartPrice(100,200,203,2000));
+
+
+
+
+const user = {
+    username:"Naman",
+    price:200
+}
+
+const users ={
+    username:"Animesh",
+    price:2000
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(users)
+ 
+const myNewArray = [200,300,30000,4000]
+
+function returnsecondValue(getArray){
+    return getArray[3]
+}
+
+console.log(returnsecondValue(myNewArray));
+
+console.log(returnsecondValue([100,200,300,400]));
