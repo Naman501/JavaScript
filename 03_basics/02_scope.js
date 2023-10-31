dc7a7cd9feb9dab51fa7d9b77ab86b5321f34263
Naman Mittal <namanmittal501@gmail.com>
2023-10-31 07:58:43 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,18 @@
+// let c = 4000
+let a = 4
+
+if(true){
+    let a =100
+    const b = 20
+    //  c =30
+    console.log(a);
+}
+
+// console.log(a);
+// console.log(b);
+console.log(a);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
