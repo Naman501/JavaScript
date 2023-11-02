9b2db1cbee9bb811d2dd31a833b59692121ca4df
Naman Mittal <namanmittal501@gmail.com>
2023-11-02 12:55:37 +0000

    if

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,1 +1,18 @@
 //if
+
+const isUserLoggedIn = true
+const temperature = 41
+
+if (3 != 2) {
+                  console.log("Maa-chuda");              //>,<,>=,<=,==,===
+}
+// console.log("abcd");
+
+if(temperature === 50){
+    console.log("less than 50.");
+}
+else{
+    console.log("temperature is grater than 50");
+}
+
+const score =200
