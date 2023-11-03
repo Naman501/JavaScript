0fca0565711db836cf7ee289e00c7262e125fdc0
Naman Mittal <namanmittal501@gmail.com>
2023-11-03 12:58:09 +0000

    switch statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "Feb"
+switch (month) {
+    case 1:
+        console.log("January"); // shift+alt+ down arrow
+        
+        break;
+    case "Feb":
+        console.log("Feb");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        // break;
+    case 4:
+        console.log("April");
+        
+        // break;
+        
+
+    default:
+        console.log("Lawda mera");
+        break;
+}
