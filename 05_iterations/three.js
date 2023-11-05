7b3a6f2e47b48f1929e7a88ee9022e924426e2b8
Naman Mittal <namanmittal501@gmail.com>
2023-11-05 13:49:37 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,46 @@
+//for of
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const Greetings = "Hello World!"
+
+for (const greet of Greetings) {
+   console.log(greet);
+    if (greet === "l") {
+        
+       
+        // console.log(greet);
+        break;
+        }
+}
+
+//Maps => maintains order and no repetition
+
+const map = new Map()
+map.set('IN',"India")
+map.set('FR',"France")
+map.set('USA',"United States Of America")
+map.set('IN',"India")
+
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key, `:-` , value);
+}
+
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key,value);
+// }
