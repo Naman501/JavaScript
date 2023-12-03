af7446d839681441f965acb17b0a7d526b06592e
Naman Mittal <namanmittal501@gmail.com>
2023-12-03 14:02:08 +0000

    Inheritance

--- 10_classes_and_oop/inheritance.js
+++ 10_classes_and_oop/inheritance.js
@@ -10,18 +10,28 @@ class User{
 }
 
 class Teacher extends User{
-    constructor(username , email , password){
+    constructor( username , email , password){
         super(username)
         this.email=email
         this.password= password
         
     }
     addCourse(){
-        // this.logMe()
         console.log(`A new course was added by ${this.username}`);
     }
 }
 
-const chai = new Teacher("chai","chai@teacher.com","1234")
+const chai = new Teacher("Bhupendra Jogi","chai@teacher.com","1234")
 
+chai.addCourse()
 chai.logMe()
+
+
+const masalaChai = new User("Osama")
+
+masalaChai.logMe()
+
+console.log(chai === masalaChai);
+console.log(chai === Teacher);
+console.log(chai  instanceof User);
+
